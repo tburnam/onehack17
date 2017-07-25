721f4253bd27e55f28bf2fc0da1e0e3d2dfe0988
Tyler <tyler@burnamtech.com>
2017-07-25 13:54:14 -0700

    Pivot

---
+++ renderer.js
@@ -0,0 +1,16 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+function test() {
+  var Horseman = require('node-horseman');
+  var horseman = new Horseman({phantomPath: "./spectrejs"});
+
+  horseman
+    .authentication('Administrator', 'T!T@n1130')
+    .userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0')
+    .open('http://10.125.227.31/contoso/m/default.aspx')
+    .injectJs('./entityToolKit.js')
+    .screenshot("test.png")
+    .close();
+
+}
